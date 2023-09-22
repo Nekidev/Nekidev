50b773be810899000b91e9b97fd2d75462704bab
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-09-22 03:26:44 -0300

    Rewrote portfolio

---
+++ nyeki/src/app/layout.js
@@ -0,0 +1,17 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+    title: "Nyeki.py",
+    description: "Hi! I'm Nyeki, a hobbyist web developer from Argentina. Here are some projects where you can find me!",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
---
+++ nyeki/src/constants.js
@@ -0,0 +1,1 @@
+export const DISCORD_USER_ID = "777338793803513886";
