34543f9934d540ed5760a02e8cec1be96d912439
Nyeki.py <84998222+Nekidev@users.noreply.github.com>
2024-08-26 19:08:18 -0300

    Update favicon.

--- nyeki/src/app/layout.js
+++ nyeki/src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { DISCORD_USER_ID } from "@/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
-                <link rel="icon" href={"https://nekosapi.com/api/discord/avatar?user_id="}>
+                <link rel="icon" href={`https://nekosapi.com/api/discord/avatar?user_id=${DISCORD_USER_ID}`}>
             </head>
             <body className={inter.className}>{children}</body>
         </html>
