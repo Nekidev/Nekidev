9106379b1d3de2e0aea4a53ff83a7437a181553d
Nyeki.py <84998222+Nekidev@users.noreply.github.com>
2024-08-26 19:09:43 -0300

    Fix typo.

--- nyeki/src/app/layout.js
+++ nyeki/src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
-                <link rel="icon" href={`https://nekosapi.com/api/discord/avatar?user_id=${DISCORD_USER_ID}`}>
+                <link rel="icon" href={`https://nekosapi.com/api/discord/avatar?user_id=${DISCORD_USER_ID}`} />
             </head>
             <body className={inter.className}>{children}</body>
         </html>
