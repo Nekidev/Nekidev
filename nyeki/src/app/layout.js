05589ac0f0f9fb43cb6c396deb344ae27db902d3
Nyeki.py <84998222+Nekidev@users.noreply.github.com>
2024-08-26 19:07:17 -0300

    Update favicon link

--- nyeki/src/app/layout.js
+++ nyeki/src/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href={"https://nekosapi.com/api/discord/avatar?user_id="}>
+            </head>
             <body className={inter.className}>{children}</body>
         </html>
     );
